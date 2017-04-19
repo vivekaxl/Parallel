1fc30e652d975a3ce2530dcb83d241514db765a1
Vivek <vivekaxl@gmail.com>
2017-04-19 04:47:12 -0400

    changed properties files

--- jmoo_properties.py
+++ jmoo_properties.py
@@ -57,9 +57,9 @@ algorithms = [
 
 problems =[
     POM3A(),
-    # POM3B(), POM3C(), POM3D(),
-    # XOMO_flight(),
-    # XOMO_all(), XOMO_ground(), XOMO_osp(), XOMO_osp2(),
+    POM3B(), POM3C(), POM3D(),
+    XOMO_flight(),
+    XOMO_all(), XOMO_ground(), XOMO_osp(), XOMO_osp2(),
     # MONRP(50, 4, 5, 0, 90), MONRP(50, 4, 5, 0, 110), MONRP(50, 4, 5, 4, 90), MONRP(50, 4, 5, 4, 110),
     # NRP(50, 4, 5, 0, 90), NRP(50, 4, 5, 0, 110), NRP(50, 4, 5, 4, 90), NRP(50, 4, 5, 4, 110),
     ]
@@ -70,9 +70,9 @@ build_new_pop = False                                       # Whether or not to 
 
 Configurations = {
     "Universal": {
-        "Repeats" : 2,
+        "Repeats" : 20,
         "Population_Size" : 100,
-        "No_of_Generations" : 3
+        "No_of_Generations" : 20
     },
     "NSGAIII": {
         "SBX_Probability": 1,
